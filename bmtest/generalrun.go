7c8e35885a4267f3ca820ff79ef8e8d85fbb7b42
Loriano Storchi <loriano@storchi.org>
2020-07-28 09:43:21 +0200

    increase waiting time , needed for old machines

--- bmtest/generalrun.go
+++ bmtest/generalrun.go
@@ -10,10 +10,10 @@ func main() {
 
 	if acc, err := bmapi.AcceleratorInit("/dev/ttyUSB1") ; err == nil {
 		for i := 0; i < 10; i++ {
-			time.Sleep(time.Millisecond * 10)
+			time.Sleep(time.Millisecond * 100)
 			fmt.Println("Sending", i)
 		        acc.BMr2o(0, uint8(i))
-		        time.Sleep(time.Millisecond * 17)
+		        time.Sleep(time.Millisecond * 100)
 		        icheck, _ := acc.BMi2r(0)
 			fmt.Println("Receiving", uint8(icheck))
 		}
