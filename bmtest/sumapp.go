64e2f8afd89dd4f4a713a2c7c7afac764859f3bc
Loriano Storchi <loriano@storchi.org>
2020-09-08 14:21:49 +0000

    go source random to added BM

---
+++ bmtest/sumapp.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"time"
+	"math/rand"
+	bmapi "git.fisica.unipg.it/bondmachine/bmapiusbuart.git"
+)
+
+func main() {
+
+	if acc, err := bmapi.AcceleratorInit("/dev/ttyUSB1") ; err == nil {
+
+		min := 0
+		max := 127
+		for i := 0; i < 10; i++ {
+			time.Sleep(time.Millisecond * 1000)
+			inv := rand.Intn(max - min) + min
+			nerr := acc.BMr2o(0, uint8(inv))
+			if nerr != nil {
+				fmt.Println("Error in send value")
+			} else  {
+				fmt.Println("Send value: ", inv)
+			}
+			time.Sleep(time.Millisecond * 1000)
+			value, _ := acc.BMi2r(0)
+			fmt.Println("Recv value: ", value)
+		}
+	}
+}
