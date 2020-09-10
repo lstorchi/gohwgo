426c05632c24e319a2a807a9d4b22a44e562ed44
Loriano Storchi <loriano@storchi.org>
2020-09-10 13:42:05 +0000

    add some extra controls to go sumapp code

--- bmtest/sumapp.go
+++ bmtest/sumapp.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"os"
 	"fmt"
 	"time"
+	"strconv"
 	"math/rand"
 	bmapi "git.fisica.unipg.it/bondmachine/bmapiusbuart.git"
 )
@@ -11,11 +13,28 @@ func main() {
 
 	rand.Seed(time.Now().UTC().UnixNano())
 
+	args := os.Args
+
+	numofloop := 10
+	if len(args) == 2 {
+		rnumofloop, err := strconv.Atoi(args[1])
+		numofloop = rnumofloop
+		if  err != nil {
+			fmt.Println("Error in atoi conversion ", err)
+			return 
+		}
+	}
+
+
 	if acc, err := bmapi.AcceleratorInit("/dev/ttyUSB1") ; err == nil {
+
+		value, _ := acc.BMi2r(0)
+		fmt.Println("Read initial value: ", value)
+		fmt.Println("")
 
 		min := 0
 		max := 127
-		for i := 0; i < 10; i++ {
+		for i := 0; i < numofloop; i++ {
 			time.Sleep(time.Millisecond * 1000)
 			inv := rand.Intn(max - min) + min
 			nerr := acc.BMr2o(0, uint8(inv))
@@ -33,5 +52,11 @@ func main() {
 				fmt.Println("Some problem accurred")
 			}
 		}
+
+		time.Sleep(time.Millisecond * 1000)
+
+		fmt.Println("")
+		value, _ = acc.BMi2r(0)
+		fmt.Println("Read Final value: ", value)
 	}
 }
